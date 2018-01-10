413f1faa3fe90ce4f58d907464033396dae3eb95
Victor Leal <victorleal18@gmail.com>
2018-01-09 22:10:26 -0200

    Implementing decision trees and setting a lower percentile to speed up training

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
+
 
 #########################################################
 
